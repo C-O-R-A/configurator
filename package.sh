104c651f06c8df889474432ab159304d826872e9
machine0herald <M.J.deLannoy@student.tudelft.nl>
2026-08-15 14:12:18 +0200

    add .deb and .rpm installers

--- package.sh
+++ package.sh
@@ -6,3 +6,11 @@ pyinstaller --onefile --name cora-backend main.py
 npm run dist:linux
 npm run dist:windows
 npm run dist:mac
+
+git tag -a v0.1.0 -m "v0.1.0"
+git push origin v0.1.0-alpha
+
+gh release create v0.1.0 \
+  "dist/CORA Configurator-0.1.0.AppImage" \
+  --title "v0.1.0" \
+  --generate-notes
